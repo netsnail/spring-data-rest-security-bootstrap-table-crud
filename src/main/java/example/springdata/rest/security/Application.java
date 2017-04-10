95b1954b9b51c70c99c1071dfbef234817953482
Tiger <tiger@netsnail.com>
2017-04-10 15:37:59 +0800

    Signed-off-by: Tiger <tiger@netsnail.com>

--- src/main/java/example/springdata/rest/security/Application.java
+++ src/main/java/example/springdata/rest/security/Application.java
@@ -28,11 +28,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * This example shows various ways to secure Spring Data REST applications using Spring Security
- *
- * @author Greg Turnquist
- */
 @SpringBootApplication
 public class Application {
 
@@ -42,57 +37,27 @@ public class Application {
 		SpringApplication.run(Application.class);
 	}
 
-	/**
-	 * Pre-load the system with employees and items.
-	 */
 	public @PostConstruct void init() {
 	}
 
-	/**
-	 * This application is secured at both the URL level for some parts, and the method level for other parts. The URL
-	 * security is shown inside this code, while method-level annotations are enabled at by
-	 * {@link EnableGlobalMethodSecurity}.
-	 *
-	 * @author Greg Turnquist
-	 * @author Oliver Gierke
-	 */
 	@Configuration
 	@EnableGlobalMethodSecurity(prePostEnabled = true)
 	@EnableWebSecurity
 	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-		/**
-		 * This section defines the user accounts which can be used for authentication as well as the roles each user has.
-		 * 
-		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder)
-		 */
 		@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-			auth.inMemoryAuthentication().//
-					withUser("user").password("user").roles("USER").and().//
+			auth.inMemoryAuthentication().
+					withUser("user").password("user").roles("USER").and().
 					withUser("admin").password("admin").roles("USER", "ADMIN");
 		}
 
-		/**
-		 * This section defines the security policy for the app.
-		 * <p>
-		 * <ul>
-		 * <li>BASIC authentication is supported (enough for this REST-based demo).</li>
-		 * <li>/employees is secured using URL security shown below.</li>
-		 * <li>CSRF headers are disabled since we are only testing the REST interface, not a web one.</li>
-		 * </ul>
-		 * NOTE: GET is not shown which defaults to permitted.
-		 *
-		 * @param http
-		 * @throws Exception
-		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
-		 */
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 
-			http.httpBasic().and().authorizeRequests().//
-					antMatchers(HttpMethod.GET, "/login").hasRole("ADMIN").//
+			http.httpBasic().and().authorizeRequests().
+					antMatchers(HttpMethod.GET, "/login").hasRole("ADMIN").
 					and().csrf().disable();
 		}
 	}
